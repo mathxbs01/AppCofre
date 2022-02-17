d76fce41bb7e1da047072d6b0beac55de14e8a9b
Matheus Santos <matheus.01bds@outlook.com>
2022-02-17 09:57:17 -0300

    Primeiro commit

---
+++ app/src/main/java/com/example/appcofre/MainActivity.java
@@ -0,0 +1,57 @@
+    package com.example.appcofre;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+    public class MainActivity extends AppCompatActivity {
+    TextView txtServicoProg;
+    EditText edtNomeProg;
+    EditText edtUsuarioProg;
+    EditText edtSenhaProg;
+    credencialModel credencial = new credencialModel();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        txtServicoProg = (TextView) findViewById(R.id.txtServico);
+        edtNomeProg = (EditText) findViewById(R.id.edtServico);
+        edtUsuarioProg = (EditText) findViewById(R.id.edtUsuario);
+        edtSenhaProg = (EditText) findViewById(R.id.edtSenha);
+    }
+    public void clickBtnDeletar(View v)
+    {
+        credencial.setNome(edtNomeProg.getText().toString());
+        credencial.setUsuario(edtUsuarioProg.getText().toString());
+        credencial.setSenha(edtSenhaProg.getText().toString());
+    }
+    public void clickBtnAlterar(View v)
+    {
+        credencial.setNome(edtNomeProg.getText().toString());
+        credencial.setUsuario(edtUsuarioProg.getText().toString());
+        credencial.setSenha(edtSenhaProg.getText().toString());
+    }
+    public void clickBtnCadastrar(View v)
+    {
+        credencial.setNome(edtNomeProg.getText().toString());
+        credencial.setUsuario(edtUsuarioProg.getText().toString());
+        credencial.setSenha(edtSenhaProg.getText().toString());
+    }
+    public void clickBtnNovo(View v)
+    {
+        limpar();
+    }
+    public void limpar()
+    {
+        edtNomeProg.setText("");
+        edtUsuarioProg.setText("");
+        edtSenhaProg.setText("");
+        txtServicoProg.setText("Serviço:");
+        edtNomeProg.requestFocus();
+    }
+}
